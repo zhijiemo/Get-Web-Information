20eef7e979be1ba5f6f076a6c3768a9c60e465d8
750282510 <750282510@qq.com>
2017-08-02 22:00:03 +0800

    推送

--- DeanSummary/Program.cs
+++ DeanSummary/Program.cs
@@ -11,31 +11,90 @@ namespace DeanSummary
     {
         static void Main(string[] args)
         {
-            var web = new HtmlWeb();
-            var doc = web.Load("http://dean.xjtu.edu.cn/jxxx/xwdt.htm");
-            var y = doc.DocumentNode.SelectSingleNode("//div[@class='list_main_content']"); 
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc1 = web.Load("http://dean.xjtu.edu.cn/jxxx/xwdt.htm");
+            HtmlDocument doc2 = web.Load("http://dean.xjtu.edu.cn/jxxx/xytz.htm");
+            HtmlDocument doc3 = web.Load("http://dean.xjtu.edu.cn/jxxx/zhtz.htm");
+            //HtmlNode  x = doc1.DocumentNode.SelectSingleNode("//div[@class='list_main_content']");
+            //HtmlNode  y = doc2.DocumentNode.SelectSingleNode("//div[@class='list_main_content']");
+            //HtmlNode  z = doc3.DocumentNode.SelectSingleNode("//div[@class='list_main_content']");
             // HtmlDocument.DocumentNode
             //              .GetElementById
             // HtmlNode.innerText .InnerHtml .SelectNodes .SelectSingleNode
-            foreach (var i in y.SelectNodes("./div"))
-            {
-                string name = null, url = null, date = null;
-                //var namenode = i.SelectSingleNode("./a");
-                //name = namenode.GetAttributeValue("title", "");
-                //Console.WriteLine(name);
-                var urlnode = i.SelectSingleNode("./a");
-                url = urlnode.GetAttributeValue("href", "");
-                //Console.WriteLine(url);
-                var datenode = i.SelectSingleNode("./span");
-                date = datenode.GetAttributeValue("datenode", "");
-                Console.WriteLine(i.InnerText  );
-                //........
-                //Console.WriteLine("({0}){1}", i.GetAttributeValue("href", ""), i.InnerText);
-                Console.WriteLine("({0}){1} {2}", url, name, date);
-            }
+            var notes = new List<Notification> ();
+            
+            //    //........
+            //    //Console.WriteLine("({0}){1}", i.GetAttributeValue("href", ""), i.InnerText);
+            //   Notification note = new Notification(name, url, date);
+
+            //    Console.WriteLine("({0}){1} {2}", url, name, date);
+            //}
+            Repetition img = new Repetition();
+            img.FindAllNotifications(doc1);
+            img.FindAllNotifications(doc2);
+            img.FindAllNotifications(doc3);
+            //notes.AddRange();
+            //notes.Add(new Notification("电气223", "abc1", "223"));
+            //notes.Add(new Notification("电气123", "abc", "123"));
+            //notes.Add(new Notification("交大", "abc", "123"));
+            //foreach (var item in notes.Where(n => n.Name.Contains("电气"))
+            //    .OrderByDescending(n => n.Date))
+            //{
+            //    Console.WriteLine(item);
+            //}
             Console.ReadLine();
             //Console.WriteLine(doc);
             //Console.ReadLine();
+        }
+    }
+    class Notification
+    {
+
+        public string Name { get; set; }
+
+        public string Url { get; set; }
+
+        public string Date { get; set; }
+
+        public Notification(string name, string url, string date)
+        {
+            Name = name;
+            Url = url;
+            Date = date;
+        }
+       
+        public override string ToString()
+        {
+            //return $"[{Date}]{Name}";
+            return string.Format("[{0}]{1}", Name, Date);
+        }
+    }
+    class Repetition
+    {
+
+        private readonly HtmlWeb web = new HtmlWeb();
+
+        public IEnumerable<Notification> DownloadAllNotifications(string url)
+        {
+            var doc = web.Load(url);
+            return FindAllNotifications(doc);
+        }
+
+        public IEnumerable<Notification> FindAllNotifications(HtmlDocument document)
+        {
+            HtmlNode node = document.DocumentNode.SelectSingleNode("//div[@class='list_main_content']");
+            foreach (var i in node.SelectNodes("./div"))
+            {
+                string name = null, url = null, date = null;
+                var namenode = i.SelectSingleNode("./a");
+                name = namenode.GetAttributeValue("title", "");
+                var urlnode = i.SelectSingleNode("./a");
+                url = urlnode.GetAttributeValue("href", "");
+                var datenode = i.SelectSingleNode("./span");
+                date = datenode.InnerText;
+                yield return new Notification(name, url, date);
+            }
+
         }
     }
 }
